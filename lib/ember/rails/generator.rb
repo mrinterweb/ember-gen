58de8de5627ee71fe2888213917c3e109741ac6f
Sean McCleary <seanmcc@gmail.com>
2014-02-21 13:03:07 -0800

    bundler generated gem

---
+++ lib/ember/rails/generator.rb
@@ -0,0 +1,9 @@
+require "ember/rails/generator/version"
+
+module Ember
+  module Rails
+    module Generator
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/ember/rails/generator/version.rb
@@ -0,0 +1,7 @@
+module Ember
+  module Rails
+    module Generator
+      VERSION = "0.0.1"
+    end
+  end
+end

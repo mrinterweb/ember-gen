b77051bca5a15db4c59aa53832b5e360a6a1e080
Sean McCleary <seanmcc@gmail.com>
2014-02-24 01:44:57 -0800

    adding some comments about template generation

--- lib/ember-gen/generate.rb
+++ lib/ember-gen/generate.rb
@@ -3,8 +3,12 @@ module EmberGen
   class Generate
 
     # todo - need to figure out a way to read users preferences for 
-    #        javascript engine and dependency managment (AMD, common.js, ES6 module, global)
+    #        javascript engine and dependency management (AMD, common.js, ES6 module, global)
     #        then the correct template can be loaded.
+    #        There are two likely places I can think that it would be good to load these files.
+    #          1. config/environments/development.rb
+    #          2. from an ember-gen specific configuration file 
+    #             I don't really like this approach, as much be it is more agnostic
     TEMPLATE_PATH = File.join(File.dirname(__FILE__), 'templates')
 
     def initialize(type)
